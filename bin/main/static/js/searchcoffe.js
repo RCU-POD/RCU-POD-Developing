ae0ee07e5d685d7b074349ed3dea5e54a20df935
Fernando <fperezc9395@hotmail.com>
2019-03-08 00:58:22 -0600

    UPDATE - Ready deployment environment and error screens fixed

--- bin/main/static/js/searchcoffe.js
+++
@@ -1,9 +0,0 @@
-var cafe =[ 
-    {"name": "Bola de Oro", "code": "BO"}, 
-    {"name": "Café Chiquito", "code": "CC"}, 
-    {"name": "REFORMANDA -Barra de café", "code": "BC"}, 
-    {"name": "Kariva Café", "code": "KC"}, 
-    {"name": "Café Don Justo", "code": "CJ"}, 
-    {"name": "Café Cali", "code": "CI"}, 
-    {"name": "Café Lago", "code": "CL"}
-  ]
---
+++ src/main/java/hello/myErrorController.java
@@ -0,0 +1,72 @@
+package hello;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+
+@Controller
+public class myErrorController implements ErrorController {
+	
+	//This two methods are related to the main page "index.html" where is all the content about coffeebenefits
+	@RequestMapping("/")
+	public String HandleError(HttpServletRequest request)
+	{
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		
+		if(status != null)
+		{
+			Integer statusCode = Integer.valueOf(status.toString());
+			
+			if(statusCode == HttpStatus.NOT_FOUND.value())
+			{
+				return "error-404";
+			}
+			else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value())
+			{
+				return "error-500";
+			}
+		}
+		else
+		{
+			return "../static/index";
+		}
+		return "error";
+	}
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
+	
+	//This two methods
+	@RequestMapping("/error")
+	public String HandleError2(HttpServletRequest request)
+	{
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		
+		if(status != null)
+		{
+			Integer statusCode = Integer.valueOf(status.toString());
+			
+			if(statusCode == HttpStatus.NOT_FOUND.value())
+			{
+				return "error-404";
+			}
+			else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value())
+			{
+				return "error-500";
+			}
+		}
+		return "error";
+	}
+	
+	public String getErrorPath2()
+	{
+		return "/error";
+	}
+}
