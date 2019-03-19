f633aba8889f91fba4ae44567782336616587bda
Marco Antonio Hernandez Salas <mar09_sep@hotmail.com>
2019-03-19 09:10:35 -0600

    RCU-010 Develop Feature 1 - Welcome Page
    Preloader completed

---
+++ src/main/resources/static/js/preloader.js
@@ -0,0 +1,24 @@
+$(".slide").css("display", "none");
+$(".gallery-container").css("display", "none");
+$(".slide2").css("display", "none");
+$(".graphic-container").css("display", "none");
+$(".searchcoffe").css("display", "none");
+$(".recommendCoffeContainer").css("display", "none");
+$(".contact-sec").css("display", "none");
+$("footer").css("display", "none");
+
+$(document).ready(function(){
+    $(".triangle-wel").on( "click", function() {
+        $('.welcome').slideUp("slow",function(){
+            
+        });
+        $('.slide').show(); 
+        $('.slide2').show();
+        $('.graphic-container').show();  
+        $('.gallery-container').show();
+        $('.searchcoffe').show();
+        $('.recommendCoffeContainer').show();
+        $('.contact-sec').show();
+        $('footer').show();
+    });
+});
