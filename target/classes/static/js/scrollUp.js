2b2c70450e31e0efa7e8ef00a02c997980ae7dd7
Chaan25 <fperezc9395@hotmail.com>
2019-04-07 01:05:44 -0600

    RCU-021 - FIX - FPC -Develop Feature 8 & 9- Add social network icons and Contact form

--- target/classes/static/js/scrollUp.js
+++ target/classes/static/js/scrollUp.js
@@ -1,14 +1,14 @@
-(document).ready(function() {
+jQuery(document).ready(function() {
     var offset=250;
     var duration = 2000;
-  $(window).scroll(function(){
-      if($(this).scrollTop()>offset){
-          $('.to-top').fadeIn(duration);
+    jQuery(window).scroll(function(){
+      if(jQuery(this).scrollTop()>offset){
+          jQuery('.to-top').fadeIn(duration);
       }else{
-          $('.to-top').fadeIn(duration);
+          jQuery('.to-top').fadeIn(duration);
       }
     });
-    $('.to-top').click(function(){
-        $('body')
+    jQuery('.to-top').click(function(){
+        jQuery('body')
     })
 });
